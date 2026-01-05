2818e5bdb886fbf63cbd12a490b054342f4eddcb
emirhan-coban <emirhancob4n@gmail.com>
2026-01-05 17:24:33 +0300

    Add project files.

---
+++ CSharpEgitimKampi601/Form1.cs
@@ -0,0 +1,35 @@
+﻿using CSharpEgitimKampi601.Entities;
+using CSharpEgitimKampi601.Services;
+using System;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        CustomerOperations customerOperations = new CustomerOperations();
+        private void btnCustomerCreate_Click(object sender, EventArgs e)
+        {
+            var customer = new Customer()
+            {
+                CustomerName = txtCustomerName.Text,
+                CustomerSurname = txtCustomerSurname.Text,
+                CustomerCity = txtCustomerCity.Text,
+                CustomerBalance = decimal.Parse(txtCustomerBalance.Text),
+                CustomerShoppingCount = int.Parse(txtCustomerShoppingCount.Text)
+            };
+            customerOperations.AddCustomer(customer);
+            MessageBox.Show("Müşteri başarıyla eklendi.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ CSharpEgitimKampi601/Services/CustomerOperations.cs
@@ -0,0 +1,37 @@
+﻿using CSharpEgitimKampi601.Entities;
+using MongoDB.Bson;
+
+namespace CSharpEgitimKampi601.Services
+{
+    public class CustomerOperations
+    {
+        public void AddCustomer(Customer customer)
+        {
+            // Müşteri ekleme işlemleri burada yapılacak
+            var connection = new MongoDbConnection();
+            var customerCollection = connection.GetCustomersCollection();
+
+            var document = new BsonDocument
+            {
+                { "CustomerName", customer.CustomerName },
+                { "CustomerSurname", customer.CustomerSurname },
+                { "CustomerCity", customer.CustomerCity },
+                { "CustomerBalance", customer.CustomerBalance },
+                { "CustomerShoppingCount", customer.CustomerShoppingCount }
+            };
+            customerCollection.InsertOne(document);
+        }
+        public void UpdateCustomer()
+        {
+            // Müşteri güncelleme işlemleri burada yapılacak
+            var connection = new MongoDbConnection();
+            var customerCollection = connection.GetCustomersCollection();
+        }
+        public void DeleteCustomer()
+        {
+            // Müşteri silme işlemleri burada yapılacak
+            var connection = new MongoDbConnection();
+            var customerCollection = connection.GetCustomersCollection();
+        }
+    }
+}
